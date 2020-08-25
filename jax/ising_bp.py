c4cc33e66f318fa2c58d6b631a7ec558a08c1e22
Joonho Kim <joonho0@gmail.com>
2020-08-24 23:30:54 -0400

    remove bandwidth, add garbage collector, add new metrics, jit compilation

--- jax/ising_bp.py
+++ jax/ising_bp.py
@@ -5,9 +5,11 @@ import jax.numpy as jnp
 import wandb
 from jax.config import config
 from tqdm import tqdm
+from collections import OrderedDict
 
 import expmgr
 import qnnops
+import gc
 
 config.update("jax_enable_x64", True)
 
@@ -32,6 +34,8 @@ parser.add_argument('--seed', type=int, metavar='M', required=True,
                     help='Random seed')
 parser.add_argument('--jax-enable-x64', action='store_true',
                     help='Enable jax x64 option.')
+parser.add_argument('--no-jit', dest='use_jit', action='store_false',
+                    help='Disable jit option to loss function.')
 parser.add_argument('--exp-name', type=str, metavar='NAME', default=None,
                     help='Experiment name. If None, the default format will be used.')
 args = parser.parse_args()
@@ -43,16 +47,11 @@ sample_size = args.sample_size
 g, h = args.g, args.h
 
 if not args.exp_name:
-    args.exp_name = f'IsingBP_Q{n_qubits}R{rot_axis}BS{block_size}_g{g}h{h}_S{seed}_SN{sample_size}'
+    args.exp_name = f'Q{n_qubits}R{rot_axis}BS{block_size} - g{g}h{h} - S{seed} - SN{sample_size}'
 expmgr.init(project='IsingBP', name=args.exp_name, config=args)
 
 # Construct the hamiltonian matrix of Ising model.
 ham_matrix = qnnops.ising_hamiltonian(n_qubits, g, h)
-bandwidth = qnnops.bandwidth(ham_matrix)
-
-print(f'Bandwidth={bandwidth}')
-wandb.config.bandwidth = str(bandwidth)
-
 
 rng = jax.random.PRNGKey(seed)  # Set of random seeds for parameter sampling
 
@@ -64,27 +63,51 @@ for i in range(1, M + 1):
     def loss(_params):
         ansatz_state = qnnops.alternating_layer_ansatz(
             _params, n_qubits, block_size, n_layers, rot_axis)
-        return qnnops.energy(ham_matrix, ansatz_state) / bandwidth
+        return qnnops.energy(ham_matrix, ansatz_state)
 
+    grad_ftn = jax.grad(loss)
+    if args.use_jit:
+        grad_loss_fn = jax.jit(grad_fn)
+        
     # Collect the norms of gradients
     params, grads = [], []
     for step in tqdm(range(sample_size)):
         rng, param_rng = jax.random.split(rng)
         _, param = qnnops.initialize_circuit_params(param_rng, n_qubits, n_layers)
-        grad = jax.grad(loss)(param)
+        grad = grad_ftn(param)
         params.append(param)
         grads.append(grad)
 
     params = jnp.vstack(params)
     grads = jnp.vstack(grads)
     grad_norms = jnp.linalg.norm(grads, axis=1)
+    
+    grads_all_mean, grads_all_var = jnp.mean(grads).item(), jnp.var(grads).item()
+    grads_single_mean, grads_single_var = jnp.mean(grads[:, 0]).item(), jnp.var(grads[:, 0]).item()
+    grads_norm_mean, grads_norm_var = jnp.mean(grad_norms).item(), jnp.var(grad_norms).item()
+    
+    logging_output = OrderedDict(
+        grad_component_all_mean=grads_all_mean,
+        grad_component_all_var=grads_all_var,
+        grad_component_single_mean=grads_single_mean,
+        grad_component_single_var=grads_single_var,
+        grad_norm_mean=grads_norm_mean,
+        grad_norm_var=grads_norm_var)
+    
+    expmgr.log(step=n_layers, logging_output=logging_output)
+    
     wandb.log(
         dict(
-            gradients=wandb.Histogram(grads),
+            grad_component_all=wandb.Histogram(grads),
+            grad_component_single=wandb.Histogram(grads[:, 0]),
             grad_norm=wandb.Histogram(grad_norms)
         ),
         step=n_layers
     )
+    
     suffix = f'Q{n_qubits}L{n_layers}R{rot_axis}BS{block_size}_g{g}h{h}'
     expmgr.save_array(f'params_{suffix}.npy', params)
     expmgr.save_array(f'grads_{suffix}.npy', grads)
+
+    del params, grads
+    gc.collect()
