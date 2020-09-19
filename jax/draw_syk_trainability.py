4a81e184d569af4a7aff3ff1e24a0e69a089cba3
Jaedeok Kim <05jaedeok@gmail.com>
2020-09-20 01:35:36 +0900

    Pin the runs when drawing trainability results

--- jax/draw_syk_trainability.py
+++ jax/draw_syk_trainability.py
@@ -17,18 +17,21 @@ def retrieve_values_from_name(fname):
 
 def download_from_wandb(resdir):
     project = 'SYK4Model'
-    target_cfgs = {
-        # 'config.lr': 0.05,
-        # 'config.scheduler_name': 'constant',
-    }
+    # target_cfgs = {
+    #     'config.lr': 0.05,
+    #     'config.scheduler_name': 'constant',
+    #     'config.seed_SYK': 1
+    # }
     print(f'Downloading experiment results from {project}')
     print(f'| Results directory : {resdir}')
-    print(f'| Target constraints: {target_cfgs}')
+    # print(f'| Target constraints: {target_cfgs}')
 
     api = wandb.Api()
-    runs = api.runs(project, filters=target_cfgs)
+    # runs = api.runs(project, filters=target_cfgs)
+    run_ids = TARGET_RUN_IDS.split('\n')
     records = []
-    for run in runs:
+    for run_id in run_ids:
+        run = api.run(f'vqc-quantum/{project}/{run_id.strip()}')
         if run.state == 'finished':
             print(run.name)
             if 'eigenvalues' not in run.config:
@@ -52,6 +55,8 @@ def download_from_wandb(resdir):
             best_step = history.loss.argmin()
             min_energy_gap = np.abs(history.loss[best_step] - ground_state_energy)  # |E(\theta) - E0|
             fidelity = history['fidelity/ground'][best_step]
+            if run.config["n_qubits"] % 4 == 2:  # SYK4 is degenerated.
+                fidelity += history['fidelity/next_to_ground'][best_step]
             loss_threshold = 1e-4
             hitting_time = float('inf')
             for i, row in history.iterrows():
@@ -111,7 +116,7 @@ def draw_optimization_energy_gap(df, linestyles):
     plt.xlabel(r'$L$', fontsize=13)
     plt.ylabel(r'$| E(\mathbf{\theta}^*) - E_0 |$', fontsize=13)
     plt.grid(True, c='0.5', ls=':', lw=0.5)
-    plt.legend(loc='upper right')
+    plt.legend(loc='right')
 
     axes = plt.gca()
     axes.spines['right'].set_visible(False)
@@ -139,7 +144,7 @@ def draw_fidelity(df, linestyles):
     plt.xlabel(r'$L$', fontsize=13)
     plt.ylabel(r'$|\,\langle \psi(\mathbf{\theta^*})\, |\, \phi \rangle\, |^2$', fontsize=13)
     plt.grid(True, c='0.5', ls=':', lw=0.5)
-    plt.legend(loc='lower right')
+    plt.legend(loc='right')
 
     axes = plt.gca()
     axes.spines['right'].set_visible(False)
@@ -179,18 +184,56 @@ def draw_convergence_speed(df, linestyles):
 
 def main():
     # Draw L vs. min_loss
-    # datapath = 'results_ising_expressibility/20200830/minloss.pkl'
-    datapath = None
+    datapath = 'results_syk_vqe/20200919/minloss.pkl'
+    # datapath = None
     if datapath:
         df = pd.read_pickle(datapath)
     else:
-        resdir = Path(f'results_syk_expressibility/{datetime.now().strftime("%Y%m%d")}')
+        resdir = Path(f'results_syk_vqe/{datetime.now().strftime("%Y%m%d")}')
         df = download_from_wandb(resdir)
 
     linestyles = ['-o', '-.o', '--o', ':o']
     draw_optimization_energy_gap(df, linestyles)
     draw_fidelity(df, linestyles)
     draw_convergence_speed(df, linestyles)
+
+
+TARGET_RUN_IDS = """51vzyt5w
+1iiwwc2s
+2yk3sqgr
+1jg2ugm9
+1yswb81l
+3ss3r7xi
+18qaij9g
+9hr5qpqg
+396lm5vb
+38z0jgt1
+1c8f7jwa
+33vp1cqv
+2h71q1me
+10rpvaap
+3es61gp1
+3ujou9nf
+2jip7auo
+38giwbt9
+3473v16h
+28tut4fn
+2h7o9sr1
+3md7d2g8
+25c4kvc6
+2s8xi4ni
+1rgkqhgr
+3kez2ssw
+2bp76ggs
+2dn3p80o
+266lgh7i
+89radulq
+1k0p0mcv
+x5879m11
+37mpqzw0
+xxh8qu4y
+1vo577mm
+23re9wkz"""
 
 
 if __name__ == '__main__':
